4b0753ec8e658f4f772b3f3838909ffe2a7519fc
Strait2thePoint <122751055+strait2thepoint@users.noreply.github.com>
2023-07-12 21:30:18 -0400

    added README

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,10 +1,16 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  type Author {
+    name: String
+  }
+
   type Book {
+    _id: ID
     title: String
-    author: String
+    authors: [Author]
     description: String
+    bookId: String
     image: String
     link: String
   }
@@ -12,5 +18,17 @@ const typeDefs = gql`
   type Query {
     books: [Book]
   }
+
+  type Mutation {
+    addUser(username: String, email: String, password: String): User
+    login(email: String, password: String): User
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+  }
 `;
+
 module.exports = typeDefs;
